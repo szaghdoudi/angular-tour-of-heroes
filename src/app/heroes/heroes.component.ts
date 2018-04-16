6c56dd2a74f36730fbbd14c2fe98a7213484f788
szaghdoudi <sofian.zaghdoudi@gmail.com>
2018-04-16 23:53:27 +0200

    service-step final version

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HEROES} from '../mock-heroes';
 import {Hero} from '../hero'
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,17 +9,23 @@ import {Hero} from '../hero'
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes = [];
 
   selectedHero  : Hero;
   
-  constructor() { }
+  constructor(private heroService:HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero:Hero){
     this.selectedHero = hero;
+  }
+
+  getHeroes() : void{
+
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
